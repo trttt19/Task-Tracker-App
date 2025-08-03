429fa2dd1bb2c8e340e6d5d31fcd811449871dbb
trttt19 <toquamonir12@gmail.com>
2025-08-03 14:16:11 +0300

    task CRUD frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,37 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // The port your frontend runs on
+
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Backend server
-        changeOrigin: true, // Ensure the request appears to come from the frontend server
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove '/api' prefix
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    // {
-    //   // This rule intercepts all requests that start with /auth
-    //   // It now forwards the full path, e.g., /auth/signup
-    //   '/auth': {
-    //     // This is the target URL of your backend server
-    //     target: 'http://localhost:3000',
-    //     // This is important for virtual hosted sites
-    //     changeOrigin: true,
-    //     // The rewrite rule has been removed!
-    //     // This allows the full path to be forwarded to your backend.
-    //     secure: false,
-    //   },
-    //   // This rule intercepts all requests that start with /tasks
-    //   // It now forwards the full path, e.g., /tasks/get-all
-    //   '/tasks': {
-    //     target: 'http://localhost:3000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //     // The rewrite rule has been removed!
-    //   },
-    // },
-    // This is useful for single-page applications for direct URL access
     historyApiFallback: true
   }
 })
